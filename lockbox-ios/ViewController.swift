7003ef71b68f945b72fa54eb2c93c28ed2600c80
sashei <seheinen@gmail.com>
2017-10-23 16:14:08 -0600

    get rid of dummy calls in initialviewcontroller

--- lockbox-ios/ViewController.swift
+++ lockbox-ios/ViewController.swift
@@ -27,47 +27,4 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-    
-    @IBAction func initClicked(_ sender: Any) {
-        self.dataStore.initialized { (value) in
-            if !value {
-                print("initializing!!")
-                self.dataStore.initialize(password: "password")
-            } else {
-                print("already initialized!")
-            }
-        }
-    }
-    
-    @IBAction func buttonClicked(_ sender: Any) {
-        self.dataStore.locked(completionHandler: { (value) in
-            if value {
-                print("unlocking!")
-                self.dataStore.unlock(password: "password")
-            } else {
-                print("unlocked!")
-            }
-        })
-    }
-    
-    @IBAction func lockButtonClicked(_ sender: Any) {
-        print("locking!")
-        self.dataStore.lock()
-    }
-    
-    @IBAction func addClicked(_ sender: Any) {
-        let item = Item.Builder()
-            .entry(ItemEntry.Builder().type("fart").build())
-            .origins(["www.neopets.com"])
-            .build()
-        self.dataStore.addItem(item)
-    }
-    
-    @IBAction func listButtonClicked(_ sender: Any) {
-        self.dataStore.keyList { (array) in
-            for item in array {
-                print(item.origins)
-            }
-        }
-    }
 }
