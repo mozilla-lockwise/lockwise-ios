70700b961978f488870e772dc09239c751fd94d7
sashei <seheinen@gmail.com>
2017-10-23 16:01:52 -0600

    Update item with basic typesafety

--- lockbox-ios/DataStore.swift
+++ lockbox-ios/DataStore.swift
@@ -2,86 +2,95 @@ import Foundation
 import WebKit
 
 enum DataStoreError : Error {
-    case NoIDPassedToDelete
+    case NoIDPassed
 }
 
 class DataStore : NSObject, WKNavigationDelegate {
     var webView:(WKWebView & TypedJavaScriptWebView)!
     private let dataStoreName:String!
-    
-    init<T: WKWebView>(webview: T, dataStoreName:String? = "ds") where T: TypedJavaScriptWebView {
+
+    init<T: WKWebView>(webview: T, dataStoreName:String = "ds") where T: TypedJavaScriptWebView {
         self.webView = webview
         self.dataStoreName = dataStoreName
         super.init()
-        
+
         self.webView.navigationDelegate = self
-        
+
         let baseUrl = URL(string: "file://\(Bundle.main.bundlePath)/lockbox-datastore/")!
         let path = "file://\(Bundle.main.bundlePath)/lockbox-datastore/index.html"
-        
+
         self.webView.loadFileURL(URL(string:path)!, allowingReadAccessTo: baseUrl)
     }
-    
+
     func webView(_ webView: WKWebView, didFinish navigation: WKNavigation!) {
         self.webView.evaluateJavaScript("var \(self.dataStoreName!);DataStoreModule.open().then((function (datastore) {\(self.dataStoreName!) = datastore;}));")
     }
-    
+
     func initialized(completionHandler: ((Bool) -> Void)?) {
         self.webView.evaluateJavaScriptToBool("\(self.dataStoreName!).initialized") { (value, error) in
             if value != nil {
                 completionHandler?(value!)
                 return
             }
-            
+
             completionHandler?(false)
         }
     }
-    
+
     func initialize(password:String) {
         self.webView.evaluateJavaScript("\(self.dataStoreName!).initialize({password:\"\(password)\",})")
     }
-    
+
     func locked(completionHandler: ((Bool) -> Void)?) {
         self.webView.evaluateJavaScriptToBool("\(self.dataStoreName!).locked") { (value, error) in
             if value != nil {
                 completionHandler?(value!)
                 return
             }
-            
+
             completionHandler?(false)
         }
     }
-    
+
     func unlock(password:String) {
         self.webView.evaluateJavaScript("\(self.dataStoreName!).unlock(\"\(password)\")")
     }
-    
+
     func lock() {
         self.webView.evaluateJavaScript("\(self.dataStoreName!).lock()")
     }
-    
+
     func keyList(completionHandler: (([Item]) -> Void)?) {
         self.webView.evaluateJavaScriptMapToArray("\(self.dataStoreName!).list()") { (array, error) in
             let mappedArray = array?.map({ (value) -> Item in
                 let itemDictionary = (value as! [Any])[1] as! [String:Any]
                 return Parser.itemFromDictionary(itemDictionary)
             })
-            
+
             completionHandler!(mappedArray!)
         }
     }
-    
+
     func addItem(_ item:Item) {
         let jsonItem = Parser.jsonStringFromItem(item)
-        
+
         self.webView.evaluateJavaScript("\(self.dataStoreName!).add(\(jsonItem))")
     }
-    
+
     func deleteItem(_ item:Item) throws {
         if item.id == nil {
-            throw DataStoreError.NoIDPassedToDelete
+            throw DataStoreError.NoIDPassed
         }
-        
+
         self.webView.evaluateJavaScript("\(self.dataStoreName!).delete(\"\(item.id!)\")")
+    }
+
+    func updateItem(_ item:Item) throws {
+        if item.id == nil {
+            throw DataStoreError.NoIDPassed
+        }
+        let jsonItem = Parser.jsonStringFromItem(item)
+
+        self.webView.evaluateJavaScript("\(self.dataStoreName!).update(\(jsonItem))")
     }
 }
--- lockbox-iosTests/DataStoreSpec.swift
+++ lockbox-iosTests/DataStoreSpec.swift
@@ -4,105 +4,105 @@ import WebKit
 
 @testable import lockbox_ios
 
-class DataStoreSpec : QuickSpec {
-    class FakeWebView : WKWebView, TypedJavaScriptWebView {
-        var evaluateJSToBoolCalled:Bool = false
-        var evaluateJSToStringCalled:Bool = false
-        var evaluateJSToArrayCalled:Bool = false
-        var evaluateJSCalled:Bool = false
-        
+class DataStoreSpec: QuickSpec {
+    class FakeWebView: WKWebView, TypedJavaScriptWebView {
+        var evaluateJSToBoolCalled: Bool = false
+        var evaluateJSToStringCalled: Bool = false
+        var evaluateJSToArrayCalled: Bool = false
+        var evaluateJSCalled: Bool = false
+
         var evaluateJavaScriptArgument = ""
-        
-        var loadFileUrlArgument = URL(string:"")
-        var loadFileBaseUrlArgument = URL(string:"")
-        
-        var boolCompletionHandler:((Bool?, Error?) -> Void)?
-        var stringCompletionHandler:((String?, Error?) -> Void)?
-        var arrayCompletionHandler:(([Any]?, Error?) -> Void)?
-        
+
+        var loadFileUrlArgument = URL(string: "")
+        var loadFileBaseUrlArgument = URL(string: "")
+
+        var boolCompletionHandler: ((Bool?, Error?) -> Void)?
+        var stringCompletionHandler: ((String?, Error?) -> Void)?
+        var arrayCompletionHandler: (([Any]?, Error?) -> Void)?
+
         func evaluateJavaScriptToBool(_ javaScriptString: String, completionHandler: ((Bool?, Error?) -> Void)?) {
             evaluateJSToBoolCalled = true
             evaluateJavaScriptArgument = javaScriptString
             boolCompletionHandler = completionHandler
         }
-        
+
         func evaluateJavaScriptToString(_ javaScriptString: String, completionHandler: ((String?, Error?) -> Void)?) {
             evaluateJSToStringCalled = true
             evaluateJavaScriptArgument = javaScriptString
             stringCompletionHandler = completionHandler
         }
-        
+
         func evaluateJavaScriptMapToArray(_ javaScriptString: String, completionHandler: (([Any]?, Error?) -> Void)?) {
             evaluateJSToArrayCalled = true
             evaluateJavaScriptArgument = javaScriptString
             arrayCompletionHandler = completionHandler
         }
-        
+
         override func evaluateJavaScript(_ javaScriptString: String, completionHandler: ((Any?, Error?) -> Void)? = nil) {
             evaluateJSCalled = true
             evaluateJavaScriptArgument = javaScriptString
         }
-        
+
         override func loadFileURL(_ URL: URL, allowingReadAccessTo readAccessURL: URL) -> WKNavigation? {
             loadFileUrlArgument = URL
             loadFileBaseUrlArgument = readAccessURL
-            
+
             return nil
         }
     }
-    
+
     var subject: DataStore!
     var webView: FakeWebView!
-    let dataStoreName:String = "dstore"
-    
+    let dataStoreName: String = "dstore"
+
     override func spec() {
         describe("DataStore") {
             beforeEach {
                 self.webView = FakeWebView()
                 self.subject = DataStore(webview: self.webView, dataStoreName: self.dataStoreName)
             }
-            
+
             describe(".init(webView:, dataStoreName:)") {
                 it("loads the correct local js & html") {
-                    expect(self.webView.loadFileUrlArgument).to(equal(URL(string:"file://\(Bundle.main.bundlePath)/lockbox-datastore/index.html")))
-                    expect(self.webView.loadFileBaseUrlArgument).to(equal(URL(string:"file://\(Bundle.main.bundlePath)/lockbox-datastore/")))
+                    expect(self.webView.loadFileUrlArgument).to(equal(URL(string: "file://\(Bundle.main.bundlePath)/lockbox-datastore/index.html")))
+                    expect(self.webView.loadFileBaseUrlArgument).to(equal(URL(string: "file://\(Bundle.main.bundlePath)/lockbox-datastore/")))
                 }
-                
+
                 it("initializes the webview datastore after loading the local files") {
                     self.subject.webView(self.webView, didFinish: nil)
-                    
+
                     expect(self.webView.evaluateJSCalled).to(beTrue())
                     expect(self.webView.evaluateJavaScriptArgument).to(equal("var \(self.dataStoreName);DataStoreModule.open().then((function (datastore) {\(self.dataStoreName) = datastore;}));"))
                 }
             }
-            
+
             describe(".initalized(completionHandler:)") {
-                var evaluatedValue:Bool?
+                var evaluatedValue: Bool?
                 beforeEach {
                     self.subject.initialized(completionHandler: { (value) in
                         evaluatedValue = value
                     })
                 }
-             
+
                 it("evaluates javascript to query the initialization status of the webview datastore") {
                     expect(self.webView.evaluateJSToBoolCalled).to(beTrue())
                     expect(self.webView.evaluateJavaScriptArgument).to(equal("\(self.dataStoreName).initialized"))
                 }
-                
+
                 describe("when js callback returns nil") {
                     it("calls completion handler with false") {
                         self.webView.boolCompletionHandler!(nil, nil)
                         expect(evaluatedValue).to(beFalse())
                     }
                 }
-                
+
                 describe("when js callback returns true") {
                     it("calls completion handler with true") {
                         self.webView.boolCompletionHandler!(true, nil)
                         expect(evaluatedValue).to(beTrue())
                     }
                 }
-                
+
                 describe("when js callback returns false") {
                     it("calls completion handler with false") {
                         self.webView.boolCompletionHandler!(false, nil)
@@ -110,46 +110,46 @@ class DataStoreSpec : QuickSpec {
                     }
                 }
             }
-            
+
             describe(".initialize(password:)") {
-                let password = "someLongPasswordString"
+                let password = "someLongPasswordStringWithQuote"
                 beforeEach {
                     self.subject.initialize(password: password)
                 }
-                
+
                 it("evaluates javascript to initialize the webview datastore") {
                     expect(self.webView.evaluateJSCalled).to(beTrue())
                     expect(self.webView.evaluateJavaScriptArgument).to(equal("\(self.dataStoreName).initialize({password:\"\(password)\",})"))
                 }
             }
-        
+
             describe(".locked(completionHandler:)") {
-                var evaluatedValue:Bool?
+                var evaluatedValue: Bool?
                 beforeEach {
                     self.subject.locked(completionHandler: { (value) in
                         evaluatedValue = value
                     })
                 }
-                
+
                 it("evaluates javascript to query the lock status of the webview datastore") {
                     expect(self.webView.evaluateJSToBoolCalled).to(beTrue())
                     expect(self.webView.evaluateJavaScriptArgument).to(equal("\(self.dataStoreName).locked"))
                 }
-                
+
                 describe("when js callback returns nil") {
                     it("calls completion handler with false") {
                         self.webView.boolCompletionHandler!(nil, nil)
                         expect(evaluatedValue).to(beFalse())
                     }
                 }
-                
+
                 describe("when js callback returns true") {
                     it("calls completion handler with true") {
                         self.webView.boolCompletionHandler!(true, nil)
                         expect(evaluatedValue).to(beTrue())
                     }
                 }
-                
+
                 describe("when js callback returns false") {
                     it("calls completion handler with false") {
                         self.webView.boolCompletionHandler!(false, nil)
@@ -157,35 +157,35 @@ class DataStoreSpec : QuickSpec {
                     }
                 }
             }
-        
+
             describe(".unlock(password:)") {
                 let password = "somePasswordString"
                 beforeEach {
                     self.subject.unlock(password: password)
                 }
-                
+
                 it("evalutes .unlock on the webview datastore") {
                     expect(self.webView.evaluateJSCalled).to(beTrue())
                     expect(self.webView.evaluateJavaScriptArgument).to(equal("\(self.dataStoreName).unlock(\"\(password)\")"))
                 }
             }
-            
+
             describe(".lock()") {
                 beforeEach {
                     self.subject.lock()
                 }
-                
+
                 it("evalutes .lock on the webview datastore") {
                     expect(self.webView.evaluateJSCalled).to(beTrue())
                     expect(self.webView.evaluateJavaScriptArgument).to(equal("\(self.dataStoreName).lock()"))
                 }
             }
-            
+
             describe(".addItem(item:)") {
                 let itemBuilder = Item.Builder()
-                    .origins(["www.barf.com"])
-                    .entry(ItemEntry.Builder().type("fart").build())
-                
+                        .origins(["www.barf.com"])
+                        .entry(ItemEntry.Builder().type("fart").build())
+
                 it("evalutes .add() on the webview datastore with the correctly-JSONified item when given an item without an ID") {
                     self.subject.addItem(itemBuilder.build())
                     expect(self.webView.evaluateJSCalled).to(beTrue())
@@ -196,21 +196,47 @@ class DataStoreSpec : QuickSpec {
             describe(".deleteItem(item:)") {
                 let id = "gdfkhjfdsmmmmmm12434hkd"
                 let itemBuilder = Item.Builder()
-                    .origins(["www.barf.com"])
-                    .entry(ItemEntry.Builder().type("fart").build())
-                var item:Item?
-                
-                it("evalutes .add() on the webview datastore with the correctly-JSONified item given an item with an ID") {
+                        .origins(["www.barf.com"])
+                        .entry(ItemEntry.Builder()
+                                .type("fart")
+                                .build())
+                var item: Item?
+
+                it("evaluates .delete() on the webview datastore given an item with an ID") {
                     item = itemBuilder.id(id).build()
                     try! self.subject.deleteItem(item!)
-                    
+
                     expect(self.webView.evaluateJSCalled).to(beTrue())
                     expect(self.webView.evaluateJavaScriptArgument).to(equal("\(self.dataStoreName).delete(\"\(id)\")"))
-                    }
-                
+                }
+
                 it("throws an error when given an item without an id") {
                     item = itemBuilder.build()
-                    expect { try self.subject.deleteItem(item!) }.to(throwError(DataStoreError.NoIDPassedToDelete))
+                    expect {
+                        try self.subject.deleteItem(item!)
+                    }.to(throwError(DataStoreError.NoIDPassed))
+                }
+            }
+
+            describe(".updateItem(item:)") {
+                let itemBuilder = Item.Builder()
+                        .origins(["www.mozilla.com"])
+                        .entry(ItemEntry.Builder()
+                                .type("login")
+                                .build())
+
+                it("evaluates .update() in the webview datastore") {
+                    let item = itemBuilder.id("fdsjklfdsjkldsf").build()
+                    try! self.subject.updateItem(item)
+
+                    expect(self.webView.evaluateJSCalled).to(beTrue())
+                    expect(self.webView.evaluateJavaScriptArgument).to(equal("\(self.dataStoreName).update(\(Parser.jsonStringFromItem(itemBuilder.build())))"))
+                }
+
+                it("throws an error when given an item without an id") {
+                    let item = itemBuilder.id("fdsjklfdsjkldsf").build()
+                    
+                    expect(try self.subject.updateItem(item)).to(throwError(DataStoreError.NoIDPassed))
                 }
             }
         }
