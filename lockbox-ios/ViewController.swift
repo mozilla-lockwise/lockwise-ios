e6e206357c48e872bb58beaf64f74db3295f1fb7
sashei <seheinen@gmail.com>
2017-10-24 16:28:53 -0600

    webview & datastore basics converted,\n still need to write specs for webview array and all of datastore

--- lockbox-ios/ViewController.swift
+++ lockbox-ios/ViewController.swift
@@ -1,30 +1,52 @@
 import UIKit
 import WebKit
 import Foundation
+import RxSwift
 
 class ViewController: UIViewController {
     var webView: WebView!
     var dataStore: DataStore!
-    
+    private let disposeBag = DisposeBag()
+
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         let contentController = WKUserContentController()
         let webConfig = WKWebViewConfiguration()
-        
+
         webConfig.preferences.setValue(true, forKey: "allowFileAccessFromFileURLs")
         webConfig.preferences.javaScriptEnabled = true
         webConfig.userContentController = contentController
-        
+
         self.webView = WebView(frame: .zero, configuration: webConfig)
-        
+
         self.view.addSubview(self.webView)
-        
+
         self.dataStore = DataStore(webview: self.webView)
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+
+
+    @IBAction func initClicked(_ sender: Any) {
+        self.dataStore.initialize(password: "password")
+    }
+
+    @IBAction func unlockClicked(_ sender: Any) {
+        self.dataStore.unlock(password: "password")
+    }
+
+    @IBAction func listClicked(_ sender: Any) {
+        self.dataStore.keyList().subscribe(onSuccess: { list in
+                    for item in list {
+                        print(item)
+                    }
+                }, onError: { error in
+                    print("error: \(error)")
+                })
+                .disposed(by: disposeBag)
     }
 }
