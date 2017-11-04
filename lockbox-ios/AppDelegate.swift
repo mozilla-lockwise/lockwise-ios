f72b2a479e9a0569f2db5725a40b8ac4fa97637d
Joe Gasiorek <joe.gasiorek@gmail.com>
2017-11-03 17:14:16 -0700

    Add Telemetry

--- lockbox-ios/AppDelegate.swift
+++ lockbox-ios/AppDelegate.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import Telemetry
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -16,6 +17,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        setupTelemetry()
         return true
     }
 
@@ -27,10 +29,21 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func applicationDidEnterBackground(_ application: UIApplication) {
         // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
         // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
+        Telemetry.default.recordEvent(category: TelemetryEventCategory.action, method: TelemetryEventMethod.background, object:
+            TelemetryEventObject.app)
+        Telemetry.default.recordSessionEnd()
+        
+        // Add the CorePing and FocusEventPing to the queue and schedule them for upload in the
+        // background at iOS's discretion (usually happens immediately).
+        Telemetry.default.queue(pingType: CorePingBuilder.PingType)
+        Telemetry.default.queue(pingType: FocusEventPingBuilder.PingType)
+        Telemetry.default.scheduleUpload(pingType: CorePingBuilder.PingType)
+        Telemetry.default.scheduleUpload(pingType: FocusEventPingBuilder.PingType)
     }
 
     func applicationWillEnterForeground(_ application: UIApplication) {
         // Called as part of the transition from the background to the active state; here you can undo many of the changes made on entering the background.
+        Telemetry.default.recordEvent(category: TelemetryEventCategory.action, method: TelemetryEventMethod.foreground, object: TelemetryEventObject.app)
     }
 
     func applicationDidBecomeActive(_ application: UIApplication) {
@@ -41,6 +54,27 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
     }
 
-
+    private func setupTelemetry() {
+        let telemetryConfig = Telemetry.default.configuration
+        telemetryConfig.appName = "Lockbox"
+        telemetryConfig.userDefaultsSuiteName = AppInfo.sharedContainerIdentifier
+        telemetryConfig.appVersion = AppInfo.shortVersion
+        
+        #if DEBUG
+            telemetryConfig.isCollectionEnabled = false
+            telemetryConfig.isUploadEnabled = false
+            telemetryConfig.updateChannel = "debug"
+        #else
+            telemetryConfig.isCollectionEnabled = true
+            telemetryConfig.isUploadEnabled = true
+            telemetryConfig.updateChannel = "release"
+        #endif
+        
+        Telemetry.default.add(pingBuilderType: CorePingBuilder.self)
+        Telemetry.default.add(pingBuilderType: FocusEventPingBuilder.self)
+        
+        Telemetry.default.recordSessionStart()
+        Telemetry.default.recordEvent(category: TelemetryEventCategory.action, method: TelemetryEventMethod.foreground, object: TelemetryEventObject.app)
+    }
 }
 
