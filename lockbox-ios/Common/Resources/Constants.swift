f9a81c92ed89dd28ae03ef5b194611c818caf863
sashei <sashaheinen@gmail.com>
2018-02-06 17:08:38 -0700

    Temporarily use extension redirect

--- lockbox-ios/Common/Resources/Constants.swift
+++ lockbox-ios/Common/Resources/Constants.swift
@@ -8,5 +8,5 @@ struct Constant {
     static let clientID = "1b024772203a0849"
     static let oauthHost = "oauth.accounts.firefox.com"
     static let profileHost = "profile.accounts.firefox.com"
-    static let redirectURI = "lockbox://redirect.ios"
+    static let redirectURI = "https://2aa95473a5115d5f3deb36bb6875cf76f05e4c4d.extensions.allizom.org"
 }
