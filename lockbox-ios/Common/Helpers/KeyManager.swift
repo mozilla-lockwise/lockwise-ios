6d85670234ab5d2751780a2f19de12fafe90edf8
sashei <sashaheinen@gmail.com>
2018-02-22 15:24:08 -0700

    Added cjose error handling and garbage collection

--- lockbox-ios/Common/Helpers/KeyManager.swift
+++ lockbox-ios/Common/Helpers/KeyManager.swift
@@ -5,30 +5,60 @@
 import CJose
 import Foundation
 
+struct KeyManagerError: Error {
+    let message: String
+}
+
 class KeyManager {
     private var jwk: OpaquePointer?
+    lazy private var err = UnsafeMutablePointer<cjose_err>.allocate(capacity: self.errSize)
+    lazy private var errSize = MemoryLayout<cjose_err>.size
+    lazy private var sizeTSize = MemoryLayout<size_t>.size
 
-    func getEphemeralPublicECDH() -> String {
-        if self.jwk == nil {
-            self.jwk = cjose_jwk_create_EC_random(CJOSE_JWK_EC_P_256, nil)
-        }
-
-        let jsonValue = cjose_jwk_to_json(self.jwk!, false, nil)
-
-        return String(cString: jsonValue!)
+    private var errorMessage: String {
+        return String(cString: self.err.pointee.message)
     }
 
-    func decryptJWE(_ jwe: String) -> String? {
-        let count = jwe.data(using: .utf8)!.count as size_t
-        let contentLen = UnsafeMutablePointer<size_t>.allocate(capacity: count)
-        let err = UnsafeMutablePointer<cjose_err>.allocate(capacity: count)
-
-        let cJoseJWE = cjose_jwe_import(jwe, count, nil)
-        guard let decryptedPayload = cjose_jwe_decrypt(cJoseJWE, self.jwk, contentLen, err) else {
-            return nil
+    func getEphemeralPublicECDH() throws -> String {
+        if self.jwk == nil {
+            self.jwk = cjose_jwk_create_EC_random(CJOSE_JWK_EC_P_256, self.err)
         }
 
-        return String(bytesNoCopy: decryptedPayload, length: contentLen.pointee, encoding: .utf8, freeWhenDone: true)
+        guard let jwk = self.jwk,
+              let jsonValue = cjose_jwk_to_json(jwk, false, self.err) else {
+            throw KeyManagerError(message: self.errorMessage)
+        }
+
+        return String(cString: jsonValue)
+    }
+
+    func decryptJWE(_ jwe: String) throws -> String {
+        let count = jwe.data(using: .utf8)!.count as size_t
+
+        guard let cJoseJWE = cjose_jwe_import(jwe, count, self.err) else {
+            throw KeyManagerError(message: self.errorMessage)
+        }
+
+        let contentLen = UnsafeMutablePointer<size_t>.allocate(capacity: self.sizeTSize)
+        guard let decryptedPayload = cjose_jwe_decrypt(cJoseJWE, self.jwk, contentLen, self.err) else {
+            cjose_jwe_release(cJoseJWE)
+            contentLen.deallocate(capacity: self.sizeTSize)
+            throw KeyManagerError(message: self.errorMessage)
+        }
+
+        cjose_jwe_release(cJoseJWE)
+
+        guard let decryptedJWEString = String(
+                bytesNoCopy: decryptedPayload,
+                length: contentLen.pointee,
+                encoding: .utf8,
+                freeWhenDone: true) else {
+            contentLen.deallocate(capacity: self.sizeTSize)
+            throw KeyManagerError(message: "Unable to import decrypted payload")
+        }
+
+        contentLen.deallocate(capacity: self.sizeTSize)
+        return decryptedJWEString
     }
 
     func random32() -> Data? {
@@ -41,5 +71,14 @@ class KeyManager {
         } else {
             return nil
         }
+    }
+
+    deinit {
+        self.err.deallocate(capacity: self.errSize)
+
+        guard let jwk = self.jwk else {
+            return
+        }
+        cjose_jwk_release(jwk)
     }
 }
