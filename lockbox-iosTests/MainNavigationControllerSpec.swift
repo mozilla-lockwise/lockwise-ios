3c320ebcfc3e65d1c0dd2b9ebf4d22ba741de85a
Joe Gasiorek <joe.gasiorek@gmail.com>
2018-03-21 13:25:06 -0700

    add tests for MainNavigationController

---
+++ lockbox-iosTests/MainNavigationControllerSpec.swift
@@ -0,0 +1,46 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+import Foundation
+import Quick
+import Nimble
+import RxTest
+import RxSwift
+import RxCocoa
+
+@testable import Lockbox
+
+class MainNavigationControllerSpec: QuickSpec {
+    var mainNavigationController: MainNavigationController!
+
+    override func spec() {
+        describe("MainNavigationController") {
+            beforeEach {
+                self.mainNavigationController = MainNavigationController()
+            }
+
+            it("sets item list as root") {
+                expect(self.mainNavigationController.topViewController).to(beAnInstanceOf(ItemListView.self))
+            }
+        }
+
+        describe("UINavigationController") {
+            class LightNavBarController: UIViewController {
+                override var preferredStatusBarStyle: UIStatusBarStyle {
+                    return UIStatusBarStyle.lightContent
+                }
+            }
+
+            it("defaults to default navbar theme") {
+                let navController = UINavigationController()
+                expect(navController.preferredStatusBarStyle).to(equal(UIStatusBarStyle.default))
+            }
+
+            it("respects root view controller navbar theme") {
+                let navController = UINavigationController(rootViewController: LightNavBarController())
+                expect(navController.preferredStatusBarStyle).to(equal(UIStatusBarStyle.lightContent))
+            }
+        }
+    }
+}
