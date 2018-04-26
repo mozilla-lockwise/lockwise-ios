afd96664bee172060afb2e0c5763b2a8e852c0ec
Joe Gasiorek <joe.gasiorek@gmail.com>
2018-04-26 13:51:33 -0700

    fix height on telemetry settings cell on iOS10

--- lockbox-ios/View/TableViewCell/SettingCell.swift
+++ lockbox-ios/View/TableViewCell/SettingCell.swift
@@ -19,4 +19,34 @@ class SettingCell: UITableViewCell {
 
         self.backgroundColor = highlighted ? Constant.color.tableViewCellHighlighted : .white
     }
+
+    override func systemLayoutSizeFitting(
+        _ targetSize: CGSize,
+        withHorizontalFittingPriority horizontalFittingPriority: UILayoutPriority,
+        verticalFittingPriority: UILayoutPriority) -> CGSize {
+
+        self.layoutIfNeeded()
+
+        var size = super.systemLayoutSizeFitting(
+            targetSize,
+            withHorizontalFittingPriority: horizontalFittingPriority,
+            verticalFittingPriority: verticalFittingPriority)
+
+        // This bug is fixed in iOS 11 and only appears on cells with subtitles
+        if !ProcessInfo().isOperatingSystemAtLeast(OperatingSystemVersion.init(
+            majorVersion: 11,
+            minorVersion: 0,
+            patchVersion: 0)) {
+
+            if let detailTextLabel = self.detailTextLabel,
+                let textLabel = self.textLabel {
+
+                if detailTextLabel.frame.height > textLabel.frame.height {
+                    size.height += detailTextLabel.frame.height - textLabel.frame.height
+                }
+            }
+        }
+
+        return size
+    }
 }
