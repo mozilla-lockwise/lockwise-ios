5920c31f02b6811c20734f3437fc5f47301025e6
Devin Reams <dreams@mozilla.com>
2018-07-25 12:50:45 -0600

    explicitly including Shared Bridging Header
    fixes #410
    
    copied from https://github.com/mozilla-mobile/firefox-ios/blob/master/Shared/Shared-Bridging-Header.h

--- lockbox-ios/Common/Resources/lockbox-ios-Bridging-Header.h
+++ lockbox-ios/Common/Resources/lockbox-ios-Bridging-Header.h
@@ -2,4 +2,10 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
+#ifndef Client_Shared_Bridging_Header_h
+#define Client_Shared_Bridging_Header_h
+
+#import <Foundation/Foundation.h>
 #import <CommonCrypto/CommonCrypto.h>
+
+#endif
