229b7d6fb6bb3bfa34f87bbcd9d76ccc118b83d3
irrationalagent <leifdoines@gmail.com>
2018-08-28 20:38:14 -0600

    add login and disconnect oauth events

--- lockbox-ios/Action/AccountAction.swift
+++ lockbox-ios/Action/AccountAction.swift
@@ -29,3 +29,28 @@ extension AccountAction: Equatable {
         }
     }
 }
+
+extension AccountAction: TelemetryAction {
+    var eventMethod: TelemetryEventMethod {
+        switch self {
+        case .oauthRedirect(let URL):
+            return .signIn
+        case .clear:
+            return .disconnect
+        case .oauthSignInMessageRead:
+            return .show
+        }
+    }
+
+    var eventObject: TelemetryEventObject {
+        return .account
+    }
+
+    var value: String? {
+        return nil
+    }
+
+    var extras:  [String: Any?]? {
+        return nil
+    }
+}
