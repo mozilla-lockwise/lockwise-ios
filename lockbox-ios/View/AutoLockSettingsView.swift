7415c0b0b6a449f93b49a1039e30b8023f6be7b7
Joe Gasiorek <joe.gasiorek@gmail.com>
2019-01-15 12:27:32 -0800

    set header for auto lock screen

--- lockbox-ios/View/AutoLockSettingsView.swift
+++ lockbox-ios/View/AutoLockSettingsView.swift
@@ -45,10 +45,19 @@ class AutoLockSettingView: UIViewController, UITableViewDelegate {
         self.presenter?.onViewReady()
     }
 
+    func tableView(_ tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
+        if self.view.traitCollection.horizontalSizeClass == .regular &&
+            self.view.traitCollection.verticalSizeClass == .regular {
+            return 55
+        }
+
+        return UITableView.automaticDimension
+    }
+
     func tableView(_ tableView: UITableView, viewForHeaderInSection section: Int) -> UIView? {
         let cell = UITableViewCell()
         cell.textLabel?.textColor = Constant.color.settingsHeader
-        cell.textLabel?.font = UIFont.preferredFont(forTextStyle: .body)
+        cell.textLabel?.font = UIFont.preferredFont(forTextStyle: .footnote)
         cell.textLabel?.text = Constant.string.autoLockHeader
         cell.textLabel?.textAlignment = NSTextAlignment.center
         cell.textLabel?.numberOfLines = 0
