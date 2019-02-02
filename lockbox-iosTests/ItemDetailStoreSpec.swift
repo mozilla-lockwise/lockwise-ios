d2d60c4c613888e1d341a44595cda74db6ca74fe
Joe Gasiorek <joe.gasiorek@gmail.com>
2019-02-01 19:34:42 -0800

    fix test on ci?

--- lockbox-iosTests/ItemDetailStoreSpec.swift
+++ lockbox-iosTests/ItemDetailStoreSpec.swift
@@ -166,7 +166,7 @@ class ItemDetailStoreSpec: QuickSpec {
 
                             self.expectation(for: predicate, evaluatedWith: detailIdObserver, handler: .none)
 
-                            self.waitForExpectations(timeout: 3, handler: { (error) in
+                            self.waitForExpectations(timeout: 10, handler: { (error) in
                                 if let error = error {
                                     XCTFail("waitForExpectations timeout setting detailId \(error)")
                                 }
