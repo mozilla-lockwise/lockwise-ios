b1112bd7032a2389ac4218378b850cdd174de695
sashei <sashaheinen@gmail.com>
2019-04-23 18:22:42 -0700

    install swiftlint in prebuild

--- buddybuild_prebuild.sh
+++ buddybuild_prebuild.sh
@@ -1,3 +1,5 @@
 #!/usr/bin/env bash
 echo "Setting SentryDSN to $SENTRY_DSN"
 /usr/libexec/PlistBuddy -c "Set SentryDSN $SENTRY_DSN" "lockbox-ios/Common/Resources/Info.plist"
+
+brew install swiftlint
