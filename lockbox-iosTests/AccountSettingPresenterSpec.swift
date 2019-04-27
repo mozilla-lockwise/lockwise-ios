b64a40c6794fcb7ce66148c71730b0d61d19b7a9
Joe Gasiorek <joe.gasiorek@gmail.com>
2019-04-27 16:17:26 -0700

    fix test

--- lockbox-iosTests/AccountSettingPresenterSpec.swift
+++ lockbox-iosTests/AccountSettingPresenterSpec.swift
@@ -113,7 +113,7 @@ class AccountSettingPresenterSpec: QuickSpec {
                 it("displays the alert controller") {
                     expect(self.view.displayAlertActionButtons).notTo(beNil())
                     expect(self.view.displayAlertControllerTitle).to(equal(String(format: Constant.string.confirmDialogTitle, Constant.string.productName)))
-                    expect(self.view.displayAlertControllerMessage).to(equal(Constant.string.confirmDialogMessage))
+                    expect(self.view.displayAlertControllerMessage).to(equal(String(format: Constant.string.confirmDialogMessage, Constant.string.productName))
                 }
 
                 describe("unlinkAccountObserver") {
