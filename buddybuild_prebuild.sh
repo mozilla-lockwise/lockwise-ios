17930d7dc636dff214912e9ee346180b46d88ea3
Isabel Rios <isabelrios@MacBookirios.local>
2019-04-29 12:20:53 +0200

    workaround for BB swift installation

--- buddybuild_prebuild.sh
+++ buddybuild_prebuild.sh
@@ -2,4 +2,9 @@
 echo "Setting SentryDSN to $SENTRY_DSN"
 /usr/libexec/PlistBuddy -c "Set SentryDSN $SENTRY_DSN" "lockbox-ios/Common/Resources/Info.plist"
 
+if ! [ -x "$(command -v swiftlint)" ] ; then
+echo "swiftlint is not installed, installing"
 brew install swiftlint
+else
+echo "swiftlint already installed"
+fi
