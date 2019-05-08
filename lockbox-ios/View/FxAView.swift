db94c9ddacd754b85c97f04fba6cc931c6865393
sashei <sashaheinen@gmail.com>
2019-05-08 10:29:41 -0700

    fxa title color

--- lockbox-ios/View/FxAView.swift
+++ lockbox-ios/View/FxAView.swift
@@ -78,6 +78,7 @@ extension FxAView: UIGestureRecognizerDelegate {
         leftButton.titleLabel?.font = .navigationButtonFont
         self.navigationItem.leftBarButtonItem = UIBarButtonItem(customView: leftButton)
 
+        self.navigationController?.navigationBar.titleTextAttributes = [.foregroundColor: UIColor.white]
         self.navigationItem.title = Constant.string.getStarted
         self.navigationItem.largeTitleDisplayMode = .never
 
