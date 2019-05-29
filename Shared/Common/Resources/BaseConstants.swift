d6c1d2f8bd5d2234830a312415715a5c3c60ae68
Joe Gasiorek <joeyg@users.noreply.github.com>
2019-05-29 15:43:04 -0700

    Remove product name from strings and fix more Lockbox references (#992)
    * Remove product name from strings
    
    * update product reference for Face ID prompt
    
    * export script is now in the upstream
    
    * add note about use of product name in storyboard for accessible logo
    
    * change PRODUCT_NAME so that CFBundleName is also updated throughout
    
    * more rename

--- Shared/Common/Resources/BaseConstants.swift
+++ Shared/Common/Resources/BaseConstants.swift
@@ -61,7 +61,7 @@ class Constant {
     class string {
         static let enablingAutofill = NSLocalizedString("autofill.enabling", value: "Updating AutoFill…", comment: "Text displayed while AutoFill credentials are being populated. AutoFill should be localized to match the proper name for Apple’s system feature")
         static let completedEnablingAutofill = NSLocalizedString("autofill.finished_enabling", value: "Finished updating AutoFill", comment: "Accessibility notification when AutoFill is done being enabled")
-        static let unlockPlaceholder = NSLocalizedString("unlock_placeholder", value: "This will unlock the app.", comment: "Placeholder text when the user’s email is unavailable while unlocking Lockbox, shown in Touch ID and passcode prompts")
+        static let unlockPlaceholder = NSLocalizedString("unlock_placeholder", value: "This will unlock the app.", comment: "Placeholder text when the user’s email is unavailable while unlocking the app, shown in Touch ID and passcode prompts")
         static let signInRequired = NSLocalizedString("autofill.signInRequired", value: "Sign in Required", comment: "Title for alert dialog explaining that a user must be signed in to use AutoFill.")
         static let signInRequiredBody = NSLocalizedString("autofill.signInRequiredBody", value: "You must be signed in to %@ before AutoFill will allow access to passwords within it.", comment: "Body for alert dialog explaining that a user must be signed in to use AutoFill. AutoFill should be localized to match the proper name for Apple's system feature. %1$@ and %2$@ will be replaced with the application name")
         static let ok = NSLocalizedString("ok", value: "OK", comment: "Ok button title")
