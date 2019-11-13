a3b24f534804374a11f35c7f200cacd39a850499
Kayla Galway <kayla.galway@gmail.com>
2019-11-13 16:50:32 -0700

    remove white space and make feature flag booleans more verbose

--- Shared/FeatureFlags/FeatureFlags.swift
+++ Shared/FeatureFlags/FeatureFlags.swift
@@ -1,16 +1,14 @@
 //
 //  FeatureFlags.swift
 //  Lockbox
-//
-//  Created by Kayla Galway on 11/8/19.
-//  Copyright © 2019 Mozilla. All rights reserved.
-//
+/* This Source Code Form is subject to the terms of the Mozilla Public
+* License, v. 2.0. If a copy of the MPL was not distributed with this
+* file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
 import Foundation
 
 /// Helper struct that checks if Build Configuration is Release or Debug
 struct FeatureFlagSupport {
-    
     static var isDebug: Bool {
         #if DEBUG
             return true
@@ -26,15 +24,18 @@ struct FeatureFlagSupport {
             return false
         #endif
     }
-    
 }
 
 /// Boolean flags that identify if test feature should be shown depending on whether the Build Configuration is Release or Debug
 struct FeatureFlags {
-    
     /// Feature flag for the ability to edit a credential
     static var crudEdit: Bool {
-        return FeatureFlagSupport.isDebug ? true : false
+        if FeatureFlagSupport.isDebug {
+            return true
+        } else if FeatureFlagSupport.isRelease {
+            return false
+        } else {
+            return false
+        }
     }
-    
 }
