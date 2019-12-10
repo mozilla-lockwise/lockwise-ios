a71f476205bd54cb3cbe9e016f677fa5cc18fd18
isabelrios <isabelrios@gmail.com>
2019-12-10 18:23:39 +0100

    1153-xcuitests-fix-iPad-new-fxa-login-page (#1154)
    * 1153-xcuitests-fix-iPad-new-fxa-login-page

--- LockwiseXCUITests/LockwiseXCUITests.swift
+++ LockwiseXCUITests/LockwiseXCUITests.swift
@@ -190,7 +190,7 @@ class LockwiseXCUITests: BaseTestCase {
         // There should be the correct number of matches
         let aMatches = app.tables.cells.count
         if  iPad() {
-            XCTAssertEqual(aMatches, 100)
+            XCTAssertEqual(aMatches, 100 )
         } else {
             XCTAssertEqual(aMatches, 97)
         }
