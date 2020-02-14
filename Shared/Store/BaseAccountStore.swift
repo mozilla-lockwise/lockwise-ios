3dab1db8b471a056c8f7ce269e34f4b3e3d5244a
Kayla Galway <kayla.galway@gmail.com>
2020-02-14 00:40:19 -0700

    more specific error logging

--- Shared/Store/BaseAccountStore.swift
+++ Shared/Store/BaseAccountStore.swift
@@ -80,7 +80,7 @@ class BaseAccountStore {
         NSLog("\(autofillLogPrefix) AS001 - starting to populate account information")
         guard let fxa = self.fxa else {
             errMessage = errMessage + "fxa is nil"
-            NSLog("\(autofillLogPrefix) \(errMessage)")
+            NSLog("\(autofillLogPrefix) ASError - \(errMessage)")
             sendSentryEvent(title: errMessage, error: nil)
             return
         }
@@ -88,7 +88,7 @@ class BaseAccountStore {
         fxa.getProfile { [weak self] (profile: Profile?, error) in
             self?._profile.onNext(profile)
             if profile == nil {
-                NSLog("\(autofillLogPrefix) \(errMessage) profile is nil")
+                NSLog("\(autofillLogPrefix) ASError - \(errMessage) profile is nil")
                 self?.sendSentryEvent(title: errMessage + "profile is nil", error: error)
             } else {
                 NSLog("\(autofillLogPrefix) AS003 - fxa profile is not nil")
@@ -96,7 +96,7 @@ class BaseAccountStore {
         }
 
         if !networkStore.isConnectedToNetwork {
-            NSLog("\(autofillLogPrefix) isConnectedToNetwork false - BaseAccountStore line 98")
+            NSLog("\(autofillLogPrefix) ASError - isConnectedToNetwork false - BaseAccountStore line 98")
             self._syncCredentials.onNext(OfflineSyncCredential)
             errMessage = errMessage + "isConnectedToNetwork false"
             sendSentryEvent(title: errMessage, error: nil)
@@ -118,32 +118,32 @@ class BaseAccountStore {
                 case .panic(let message):
                     errMessage = errMessage + "Panic error: " + message
                 }
-                NSLog("\(autofillLogPrefix) FxAException: \(errMessage)")
+                NSLog("\(autofillLogPrefix) ASError - FxAException: \(errMessage)")
                 self?.sendSentryEvent(title: "FxAException: " + errMessage, error: error)
                 NSLog("Unexpected error getting access token: \(error.localizedDescription)")
                 self?._syncCredentials.onNext(nil)
             } else if let error = err {
-                NSLog("\(autofillLogPrefix) Unexpected exception: \(error.localizedDescription)")
+                NSLog("\(autofillLogPrefix) ASError - Unexpected exception: \(error.localizedDescription)")
                 self?.sendSentryEvent(title: errMessage + "Unexpected exception: ", error: error)
                 self?._syncCredentials.onNext(nil)
             }
             NSLog("\(autofillLogPrefix) AS006 - no error in fxa.getAccessToken, about to unwrap key data")
             guard let key = accessToken?.key else {
-                NSLog("\(autofillLogPrefix) accessToken.key is nil")
+                NSLog("\(autofillLogPrefix) ASError - accessToken.key is nil")
                 self?.sendSentryEvent(title: errMessage + "key is nil", error: nil)
                 self?._syncCredentials.onNext(nil)
                 return
             }
             NSLog("\(autofillLogPrefix) AS007 - unwrapped accessToken.key successfully")
             guard let token = accessToken?.token else {
-                NSLog("\(autofillLogPrefix) accessToken.token is nil")
+                NSLog("\(autofillLogPrefix) ASError - accessToken.token is nil")
                 self?.sendSentryEvent(title: errMessage + "token is nil", error: nil)
                 self?._syncCredentials.onNext(nil)
                 return
             }
             NSLog("\(autofillLogPrefix) AS008 - unwrapped accessToken.token successfully")
             guard let tokenURL = try? self?.fxa?.getTokenServerEndpointURL() else {
-                NSLog("\(autofillLogPrefix) tokenURL is nil")
+                NSLog("\(autofillLogPrefix) ASError - tokenURL is nil")
                 self?.sendSentryEvent(title: errMessage + "tokenURL is nil", error: nil)
                 self?._syncCredentials.onNext(nil)
                 return
@@ -165,7 +165,7 @@ class BaseAccountStore {
                 NSLog("\(autofillLogPrefix) AS012 - fxa.toJSON successful, setting KeychainKey")
                 self?.keychainWrapper.set(accountJSON, forKey: KeychainKey.accountJSON.rawValue)
             } catch {
-                NSLog("\(autofillLogPrefix) fxa.toJSON failed: \(error.localizedDescription)")
+                NSLog("\(autofillLogPrefix) ASError - fxa.toJSON failed: \(error.localizedDescription)")
                 self?.sendSentryEvent(title: errMessage + "fxa to JSON failed", error: error)
             }
             NSLog("\(autofillLogPrefix) AS013 - leaving populationAccountInformation()")
