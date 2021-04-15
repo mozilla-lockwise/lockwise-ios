4d68f1283558a20e240527a8235c779becb1aa08
Stefan Arentz <stefan@arentz.ca>
2021-04-15 16:48:16 -0400

    Simplify the build process (#1249)

--- buddybuild_prebuild.sh
+++ buddybuild_prebuild.sh
@@ -2,23 +2,3 @@
 echo "Setting SentryDSN to $SENTRY_DSN"
 /usr/libexec/PlistBuddy -c "Set SentryDSN $SENTRY_DSN" "lockbox-ios/Common/Resources/Info.plist"
 
-if ! [ -x "$(command -v swiftlint)" ] ; then
-echo "swiftlint is not installed, installing"
-brew install swiftlint
-else
-echo "swiftlint already installed"
-fi
-
-if [ "$BUDDYBUILD_SCHEME" = "uispecs" ]; then
-
-# Check if python is installed
-python3 --version
-
-cd scripts/
-
-echo "Password: $TEST_EMAIL_PASSWORD"
-# Install cryptography
-pip3 install PyFxA syncclient cryptography
-# Run script to upload new login
-python3 upload_fake_passwordsBB.py 1
-fi
